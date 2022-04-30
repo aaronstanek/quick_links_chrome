3cea9666013eeb5ba2875385337e5c3e1aef8e61
Aaron Stanek <amr.stanek@gmail.com>
2022-04-30 16:51:07 -0700

    omnibox functional example

--- src/background.js
+++ src/background.js
@@ -1,13 +1,10 @@
-chrome.omnibox.onInputEntered.addListener((url, disposition) => {
-    switch (disposition) {
-      case "currentTab":
+function repalce(str) {
+    return "page.html";
+}
+
+chrome.omnibox.onInputEntered.addListener(
+    (url, disposition) => {
+        url = repalce(url);
         chrome.tabs.update({url});
-        break;
-      case "newForegroundTab":
-        chrome.tabs.create({url});
-        break;
-      case "newBackgroundTab":
-        chrome.tabs.create({url, active: false});
-        break;
     }
-  });
+);
