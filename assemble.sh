4ef5a431640f83e16766614c0af1e989eda9cc65
Aaron Stanek <amr.stanek@gmail.com>
2022-05-04 11:12:56 -0700

    basic example of data loading is functional

--- assemble.sh
+++ assemble.sh
@@ -2,3 +2,4 @@ mkdir -p src/links/build
 cp links/public/index.html src/links
 cp links/public/build/bundle.css src/links/build
 cp links/public/build/bundle.js src/links/build
+cp links/public/build/bundle.js.map src/links/build
--- links/src/App.svelte
+++ links/src/App.svelte
@@ -1,7 +1,23 @@
 <script>
+    let message;
+    function update() {
+        chrome.storage.local.get(["links"],(result)=>{
+            if (chrome.runtime.lastError) {
+                message = "An error occurred while loading data.";
+            }
+            else if (typeof result.links !== "object") {
+                message = "No links to show yet.";
+            }
+            else {
+                message = "Data was loaded.";
+            }
+        });
+    }
+    update();
 </script>
 
 <main>
+    <p>{message}</p>
 </main>
 
 <style>
