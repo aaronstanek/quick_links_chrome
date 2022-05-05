a1e90a44edd956ecc09044f5044442d6b01025a7
Aaron Stanek <amr.stanek@gmail.com>
2022-05-05 13:51:50 -0700

    alphabetized data in table

--- links/src/Table.svelte
+++ links/src/Table.svelte
@@ -1,8 +1,30 @@
 <script>
     export let linkTable;
+    let rows = [];
+    function createRows(table) {
+        if (typeof table !== "object") {
+            return [];
+        }
+        let keys = Object.keys(table).sort();
+        let output = [];
+        for (let i = 0; i < keys.length; ++i) {
+            let key = keys[i];
+            output.push([key,linkTable[key]]);
+        }
+        return output;
+    }
+    $: rows = createRows(linkTable);
 </script>
 
-<p>{JSON.stringify(linkTable)}</p>
+<div class="table">
+    {#each rows as row}
+        <p>{JSON.stringify(row)}</p>
+    {/each}
+</div>
 
 <style>
+    .table {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
