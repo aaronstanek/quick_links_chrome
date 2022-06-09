f4eed33bd2c9fb288302e8e7cabffc0ef46bcb8a
Aaron Stanek <amr.stanek@gmail.com>
2022-06-09 10:53:31 -0700

    handleQuick

--- links/src/create.js
+++ links/src/create.js
@@ -11,7 +11,7 @@ class handleURLresult {
 function handleURL(urltext) {
     // check the format
     let output = new handleURLresult();
-    if (urltext.slice(0,7) != "http://" && urltext.slice(0,8) != "https://") {
+    if (urltext.slice(0,7) !== "http://" && urltext.slice(0,8) !== "https://") {
         return output;
     }
     urltext = urltext.split("%x").join("%X");
@@ -22,7 +22,7 @@ function handleURL(urltext) {
     let sections = urltext.split("%X");
     // sections 1 and onward must begin with a numeral
     for (let i = 1; i < sections.length; ++i) {
-        if (sections[i].length == 0) {
+        if (sections[i].length === 0) {
             return output;
         }
         if (sections[i].charCodeAt(0) < 48 || sections[i].charCodeAt(0) > 57) {
@@ -41,5 +41,59 @@ function handleURL(urltext) {
     // and rewritten the secions so that hey can be read by the redirection algorithm later
     output.valid = true;
     output.url = sections.join("");
+    return output;
+}
+
+class handleQuickresult {
+    constructor() {
+        this.valid = false;
+        this.link = "";
+        this.variables = [];
+    }
+}
+
+function handleQuick(quicktext) {
+    // check the format
+    let output = new handleQuickresult();
+    let sections = quicktext.split("/");
+    let seenVariable = false;
+    for (let i = 0; i < sections.length; ++i) {
+        if (sections[i].length === 0) {
+            return output;
+        }
+        sections[i] = sections[i].split("%x").join("%X");
+        if (sections[i].split("%x").length > 1) {
+            // it contains %X
+            if (sections[i].length !== 3) {
+                return output;
+            }
+            let varname = sections[i][2];
+            if (varname.charCodeAt(0) < 48 || varname.charCodeAt(0) > 57) {
+                // variable name is not a numeral
+                return output;
+            }
+            // check to see if this variable has appeared before
+            for (let j = 0; j < output.variables.length; ++j) {
+                if (output.variables[j] === varname) {
+                    // reuse of variable
+                    return output;
+                }
+            }
+            // variable name is new
+            output.variables.push(varname);
+            seenVariable = true;
+        }
+        else {
+            // this is not a variable section
+            // make sure that there are no prior variable sections
+            if (seenVariable) {
+                return output;
+            }
+        }
+    }
+    // all validated
+    // all variables have been recorded
+    output.valid = true;
+    output.link = sections.slice(0,sections.length-output.variables.length).join("/");
     return output;
 }
