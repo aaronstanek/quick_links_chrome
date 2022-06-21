191f358c1377a00e3937df05b0e31be60a5c1fb9
Aaron Stanek <amr.stanek@gmail.com>
2022-06-21 16:09:09 -0700

    completed nomatch page

--- nomatch/src/App.svelte
+++ nomatch/src/App.svelte
@@ -1,9 +1,25 @@
 <script>
     'use strict';
+    let link = "";
+    function getLink() {
+        let params = new URLSearchParams(window.location.search);
+        link = params.get("link");
+    }
+    getLink();
 </script>
 
 <main>
+    <p>
+        Unable to find the quick link:
+        <br><br>
+        {link}
+        <br><br>
+        <a href={"../links/index.html?create="+encodeURIComponent(link)}>Click here to create it.</a>
+    </p>
 </main>
 
 <style>
+    p {
+        text-align: center;
+    }
 </style>
