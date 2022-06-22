244acf8581d2e825aed75f921eeddb61a736c8b2
Aaron Stanek <amr.stanek@gmail.com>
2022-06-22 06:21:33 -0700

    created HTML and CSS for import page

--- import/src/App.svelte
+++ import/src/App.svelte
@@ -1,9 +1,46 @@
 <script>
     'use strict';
+    let importBar;
+    function doImport() {
+
+    }
 </script>
 
 <main>
+    <div class="table">
+        <div class="row">
+            <input class="in" type="text" bind:this={importBar} />
+            <p class="spacer"></p>
+            <p class="import" on:click={doImport}>IMPORT</p>
+        </div>
+    </div>
 </main>
 
 <style>
+    .table {
+        display: flex;
+        flex-direction: column;
+        width: 61.8%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .row {
+        display: flex;
+        box-sizing: border-box;
+    }
+    .spacer {
+        flex: 1;
+    }
+    .in {
+        border-radius: 100px;
+        flex: 31;
+    }
+    .import {
+        flex: 5;
+        border: 2px solid black;
+        border-radius: 15px;
+        text-align: center;
+        background-color: #4dff4d;
+        cursor: pointer;
+    }
 </style>
