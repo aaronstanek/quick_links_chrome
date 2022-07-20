ecccfcb7f8ca292be146e5d92ccac190cc7d8b20
Aaron Stanek <amr.stanek@gmail.com>
2022-07-20 15:14:11 -0700

    finished installing build system

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var path = require('path')
+var tar = require('tar')
 var piebuilder = require('piebuilder')
 
 var project = new piebuilder.Project()
@@ -24,6 +25,7 @@ function makeDir(directory) {
 }
 
 project.beforeTask(makeDir('out'))
+project.beforeTask(makeDir('intermediate'))
 for (let i = 0; i < pageList.length; ++i) {
     project.beforeTask(makeDir(path.join('out',pageList[i])))
 }
@@ -34,57 +36,81 @@ projectContext.fileDependency('build.js')
     .fileDependency('package.json')
     .fileDependency('package-lock.json')
 
-
 function createRootTarget() {
     var t = projectContext.target(piebuilder.makeVirtualPath('root_target'))
-    t.fileDependency(piebuilder.makeVirtualPath('global'))
+    t.fileDependency(piebuilder.makeVirtualPath('out_global'))
     for (let i = 0; i < pageList.length; ++i) {
-        t.fileDependency(piebuilder.makeVirtualPath('page_' + pageList[i]))
+        t.fileDependency(piebuilder.makeVirtualPath('out_'+pageList[i]))
     }
 }
 
 createRootTarget()
 
-function copyContent(source,destination) {
-    if (piebuilder.sys.family === 'unix') {
-        return 'cp -R \"' + source + '\"/* \"' + destination + '\"'
-    }
-    else {
-        return 'xcopy \"' + source + '\" \"' + destination + '\\\" /E/Y'
-    }
-}
+projectContext.target(piebuilder.makeVirtualPath('out_global'))
+    .fileDependency(path.join('intermediate','global.tar'))
+    .task(()=>{
+        tar.x({
+                sync: true,
+                file: path.join('intermediate','global.tar'),
+                cwd: 'out'
+        })
+        return 0
+    })
 
-function globalCopy() {
-    var t = projectContext.target(piebuilder.makeVirtualPath('global'))
-    t.directorySourcesDependency(path.join('src','global'))
-    t.task(copyContent(
-        path.join('src','global'),
-        path.join('out')
-    ))
-}
-
-globalCopy()
-
-function pageTarget(pageName) {
-    var t = projectContext.target(piebuilder.makeVirtualPath('page_'+pageName))
-    t.directorySourcesDependency(
-        path.join('src','pages',pageName,'src')
-    )
-    var staticFiles = fs.readdirSync(path.join('src','pages',pageName,'public'))
-    for (let i = 0; i < staticFiles.length; ++i) {
-        if (staticFiles[i] !== 'build') {
-            t.fileDependency(path.join('src','pages',pageName,'public',staticFiles[i]))
-        }
-    }
-    t.task('cd \"' + path.join('src','pages',pageName) + '\" && npm run build')
-    t.task(copyContent(
-        path.join('src','pages',pageName,'public'),
-        path.join('out',pageName)
-    ))
+function unTar(pageName) {
+    projectContext.target(piebuilder.makeVirtualPath('out_'+pageName))
+        .fileDependency(path.join('intermediate',pageName+'.tar'))
+        .task(()=>{
+            tar.x({
+                sync: true,
+                file: path.join('intermediate',pageName+'.tar'),
+                cwd: path.join('out',pageName)
+            })
+            return 0
+        })
 }
 
 for (let i = 0; i < pageList.length; ++i) {
-    pageTarget(pageList[i])
+    unTar(pageList[i])
+}
+
+projectContext.target(path.join('intermediate','global.tar'))
+    .directorySourcesDependency(path.join('src','global'))
+    .task(()=>{
+        tar.c({
+            sync: true,
+            file: path.join('intermediate','global.tar'),
+            cwd: path.join('src','global')
+        },
+        fs.readdirSync(path.join('src','global'))
+        )
+        return 0
+    })
+
+function buildPage(pageName) {
+    var t = projectContext.target(path.join('intermediate',pageName+'.tar'))
+    t.directorySourcesDependency(path.join('src','pages',pageName,'src'))
+    var publicFiles = fs.readdirSync(path.join('src','pages',pageName,'public'))
+    for (let i = 0; i < publicFiles.length; ++i) {
+        if (publicFiles[i] !== 'build') {
+            t.fileDependency(path.join('src','pages',pageName,'public',publicFiles[i]))
+        }
+    }
+    t.task('cd \"' + path.join('src','pages',pageName) + '\" && npm run build')
+    t.task(()=>{
+        tar.c({
+            sync: true,
+            file: path.join('intermediate',pageName+'.tar'),
+            cwd: path.join('src','pages',pageName,'public')
+        },
+        publicFiles     
+        )
+        return 0
+    })
+}
+
+for (let i = 0; i < pageList.length; ++i) {
+    buildPage(pageList[i])
 }
 
 var buildOutput = project.build(piebuilder.makeVirtualPath('root_target'))
@@ -92,6 +118,7 @@ var buildOutput = project.build(piebuilder.makeVirtualPath('root_target'))
 console.log('')
 
 let fileList = Object.keys(buildOutput.files)
+fileList.sort()
 for (let i = 0; i < fileList.length; ++i) {
     let fileName = fileList[i]
     console.log(fileName,buildOutput.files[fileName])
