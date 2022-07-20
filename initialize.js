72885650bfb2a776fb84acc8347ef9952c7fef7d
Aaron Stanek <amr.stanek@gmail.com>
2022-07-20 10:39:30 -0700

    wrote initialize.js

---
+++ initialize.js
@@ -0,0 +1,26 @@
+var fs = require('fs')
+var platform = require('os').platform()
+var path = require('path')
+var piebuilder = require('piebuilder')
+
+var init = new piebuilder.Project()
+
+function npmInstallEachPage() {
+    var pageList = fs.readdirSync(path.join('src','pages'))
+    for (let i = 0; i < pageList.length; ++i) {
+        var pageName = pageList[i]
+        var directory = path.join(path.join('src','pages',pageName))
+        if (platform === 'win32') {
+            init.beforeTask('cd ' + directory + ' && npm install')
+        }
+        else {
+            init.beforeTask('cd ' + directory + ' ; npm install')
+        }
+    }
+}
+
+npmInstallEachPage()
+
+init.target(piebuilder.makeVirtualPath('init_target'))
+
+init.build(piebuilder.makeVirtualPath('init_target'))
