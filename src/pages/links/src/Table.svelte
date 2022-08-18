1f962ed3a40c8b26f1eeda163bb33b0cf0d84dcc
Aaron Stanek <amr.stanek@gmail.com>
2022-08-18 15:46:12 -0700

    The links that you create appear here.

--- src/pages/links/src/Table.svelte
+++ src/pages/links/src/Table.svelte
@@ -7,9 +7,23 @@
     export let create; // pass create operation upwards
     export let pushDisableEdit; // pass editing disable upwards
     export let editLink;
+    let rowSpanElem;
+    let emptyMessageElem;
     let rows = [];
-    function createRows(table) {
+    function showRowSpan(rse,eme) {
+        rse.style.display = "inline";
+        eme.style.display = "none";
+    }
+    function showEmptyMessage(rse,eme) {
+        rse.style.display = "none";
+        eme.style.display = "block";
+    }
+    function createRows(table,rse,eme) {
+        if (typeof rse === "undefined" || typeof eme === "undefined") {
+            return [];
+        }
         if (typeof table !== "object") {
+            showEmptyMessage(rse,eme);
             return [];
         }
         let keys = Object.keys(table).sort();
@@ -19,16 +33,25 @@
             let key = keys[i];
             output.push([key,linkTable[key]]);
         }
+        if (output.length) {
+            showRowSpan(rse,eme);
+        }
+        else {
+            showEmptyMessage(rse,eme);
+        }
         return output;
     }
-    $: rows = createRows(linkTable);
+    $: rows = createRows(linkTable,rowSpanElem,emptyMessageElem);
 </script>
 
 <div class="table">
     <Input createHandler={create}></Input>
+    <span bind:this={rowSpanElem}>
     {#each rows as row}
         <Row content={row} trash={trash} pushDisableEdit={pushDisableEdit} editLink={editLink}></Row>
     {/each}
+    </span>
+    <p class="emptymessage" bind:this={emptyMessageElem}>The links that you create appear here.</p>
 </div>
 
 <style>
@@ -38,5 +61,9 @@
         width: 61.8%;
         margin-left: auto;
         margin-right: auto;
+    }
+    .emptymessage {
+        text-align: center;
+        filter: drop-shadow(0px 6px 4px #989898);
     }
 </style>
